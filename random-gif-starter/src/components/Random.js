c7beb3e2482123dbbcc2351e8845b8ef1fc6fccf
TanviChandak <tanvichandak18@gmail.com>
2024-01-24 05:03:38 +0530

    changed design

--- random-gif-starter/src/components/Random.js
+++ random-gif-starter/src/components/Random.js
@@ -1,49 +1,24 @@
-
+import React from 'react';
 import Spinner from './Spinner';
 import useGif from '../hooks/useGif';
 
 const Random = () => {
-  // const [loader, setSpinner] = useState(false);
-  // const [gif, setGif] = useState('');
-
-  // async function fetchUrl() {
-  //   setSpinner(true);
-  //   var url = `https://api.giphy.com/v1/gifs/random?apikey=${process.env.REACT_APP_GIPHY_API_KEY}`;
-  //   try {
-  //     const { data } = await axios.get(url);
-  //     const gifUrl = data.data.images.downsized_large.url;
-  //     setGif(gifUrl);
-  //   } catch (error) {
-  //     console.error('Error fetching GIF:', error);
-  //   } finally {
-  //     setSpinner(false);
-  //   }
-  // }
-
-  // useEffect(() => {
-  //   fetchUrl();
-  // }, []);
-
-  const {loader,gif,fetchUrl} = useGif()
-
-  // function btnclickHandler() {
-  //   fetchUrl();
-  // }
+  const { loader, gif, fetchUrl } = useGif();
 
   return (
-    <div className="bg-green-500 w-3/4 mx-auto mt-10 p-4 text-center rounded-lg">
-      <div className="font-bold text-2xl mb-4">A Random Gif</div>
-      <div className="flex justify-center items-center">
-        {loader ? <Spinner /> : <img src={gif} alt="Random Gif" className="max-w-full rounded-lg" />}
+    <div className="bg-purple-500 min-h-screen flex flex-col items-center justify-center text-white">
+      <div className="bg-pink-500 w-3/4 p-8 text-center rounded-lg shadow-lg">
+        <h1 className="font-extrabold text-4xl mb-4">✨ A Random Gif Surprise! ✨</h1>
+        <div className="flex justify-center items-center">
+          {loader ? <Spinner /> : <img src={gif} alt="Random Gif" className="max-w-full rounded-lg" />}
+        </div>
+        <button
+          onClick={fetchUrl}
+          className="mt-4 bg-pink-700 text-white px-6 py-3 rounded-full hover:bg-pink-800 transition duration-300"
+        >
+          Discover Another Surprise
+        </button>
       </div>
-      <button
-        onClick={() =>{
-          fetchUrl();
-        }}
-        className="bg-white opacity-90 px-4 py-2 mt-4 rounded-md hover:bg-gray-300 transition duration-300"
-      >
-        Generate GIF
-      </button>
     </div>
   );
 };
